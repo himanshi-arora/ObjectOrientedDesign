ac0aa0db8067e349563384c27f5e252c5fc7fb10
Himi Arora <himanshi07a@gmail.com>
2020-07-13 03:33:55 +0530

    Making superclass Biycycle abrstract and extending it in RoadBike and MountainBike class

--- Implementaion/Implementation_CH_4.java
+++ Implementaion/Implementation_CH_4.java
@@ -4,26 +4,19 @@ javac -version
 javac Implementation_CH_4.java -d ClassFiles
 java -cp ClassFiles OOD.Implementation_CH_4
 */
-class Bicycle
+abstract class Bicycle{
+	public abstract String spares();
+}
+class RoadBike extends Bicycle
 {
-	private String style,size, chain,tape_color,front_shock,rear_shock;
-    protected double tire_size;
 
-    Bicycle(String size){
-		this.style=style;
+	private String size, chain,tape_color;
+    protected double tire_size;
+	RoadBike(String size,String tape_color){
 		this.size = size;
-		this.tire_size = 2.1;
-		this.chain="10-speed";
-		this.style="mountain";
-		
-	}
-	Bicycle(String size,String front_shock,String tape_color){
-		this.style="road";
-		this.size = size;
-		this.front_shock=front_shock;
-		this.tire_size = 23;
-		this.chain="10-speed";
 		this.tape_color = tape_color;
+		this.chain = "10-speed";
+		this.tire_size = 23;
 	}
 	public String size(){
 		return size;
@@ -31,48 +24,56 @@ class Bicycle
 	public String tape_color(){
 		return tape_color;
 	}
-	public String front_shock(){
-		return front_shock;
-	}
 	public String spares(){
-		if(style.equals("road"))
-			return " chain : "+chain+" tire_size : "+tire_size+" tape_color : "+ tape_color;
-		else
-			return " chain : "+chain+" tire_size : "+tire_size+" rear_shock : ";
-
+		return " chain : "+chain+" tire_size : "+tire_size+" tape_color : "+ tape_color;
 	}
+	
 
 }
 class MountainBike extends Bicycle
 {
-	private String front_shock;
-	private String rear_shock;
+	private String size, chain,front_shock,rear_shock;
+	double tire_size;
 	MountainBike(String size,String front_shock,String rear_shock)
 	{
-		super(size);
+		this.size=size;
 		this.front_shock=front_shock;
 		this.rear_shock=rear_shock;
+		this.tire_size = 2.1;
+		this.chain="10-speed";
 		
+	}
+	public String size(){
+		return size;
 	}
 	public String front_shock(){
 		return front_shock;
 	}
+	public String rear_shock(){
+		return front_shock;
+	}
 	public String spares(){
 		
-		return super.spares()+ rear_shock;
-
+		return " chain : "+chain+" tire_size : "+tire_size+" rear_shock : "+rear_shock;
 	}
 }
 public class Implementation_CH_4{
 	public static void main(String[] args)
 	{
-		MountainBike bike=new MountainBike("S","Manitou","Fox");
-		System.out.println("Bike.size :"+ bike.size());
-		System.out.println("Bike.spares :\n"+ bike.spares());
+		MountainBike mbike=new MountainBike("S","Manitou","Fox");
+		System.out.println("MounttainBike size :"+ mbike.size());
+		System.out.println("MountainBike spares :\n"+ mbike.spares());
+
+		RoadBike rbike=new RoadBike("M","red");
+		System.out.println("RoadBike size :"+ rbike.size());
+		System.out.println("RoadBike spares :\n"+ rbike.spares());
 
 		// Output:
-		// Bike.size :S
-		// Bike.spares :
+		// MounttainBike size :S
+		// MountainBike spares :
 		//  chain : 10-speed tire_size : 2.1 rear_shock : Fox
+		// RoadBike size :M
+		// RoadBike spares :
+		//  chain : 10-speed tire_size : 23.0 tape_color : red
 	}
 }
