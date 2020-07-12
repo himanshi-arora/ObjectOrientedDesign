4689c82279c0991bec7270e8025df9286a522af4
Himi Arora <himanshi07a@gmail.com>
2020-07-13 01:38:39 +0530

    Initial Class for Understanding Dependencies

---
+++ Implementaion/Implementation_CH_3.java
@@ -0,0 +1,62 @@
+package OOD;
+/*
+javac -version
+javac Implementation_CH_2.java -d ClassFiles
+java -cp ClassFiles OOD.Implementation_CH_2
+*/
+class Wheel {
+
+	private double diameterOfRim;
+	private double diameterOfTire;
+
+	Wheel( double diameterOfRim, double diameterOfTire){
+		this.diameterOfRim = diameterOfRim;
+		this.diameterOfTire = diameterOfTire;
+	}
+
+	public double diameter(){
+		return (diameterOfRim + (diameterOfTire *2));
+	}	
+	public double circumference(){
+        return Math.PI * diameter();
+    }
+}
+class Gear{
+	private int not_chainring;  //number of teeth in chaining
+	private int not_cog;		//number of teeth in cog
+	private double diameterOfRim;
+	private double diameterOfTire;
+
+	public Gear(int not_chainring, int not_cog, double diameterOfRim, double diameterOfTire){
+		this.not_chainring=not_chainring;
+		this.not_cog=not_cog;
+		this.diameterOfRim = diameterOfRim;
+		this.diameterOfTire = diameterOfTire;
+	}
+	private int not_cog()
+	{
+		return not_cog;
+	}
+	private int not_chainring()
+	{
+		return not_chainring;
+	}
+	public double ratio(){
+		return (double)not_chainring()/(double) not_cog();
+	}
+	
+	public double gear_inches(){
+		return this.ratio() * (new Wheel(diameterOfRim,diameterOfTire).diameter());
+	}
+}
+public class Implementation_CH_3{
+	public static void main(String[] args)
+	{
+		
+		Gear object1=new Gear(52,11,26,1.5);
+		System.out.println("Gear_Inches :"+ object1.gear_inches()); 
+
+		// Output:
+		// Gear_Inches :137.0909090909091
+	}
+}
