b92b0485ed54c8d5ffb47a225dedcec2970b11c7
Himi Arora <himanshi07a@gmail.com>
2020-07-13 03:53:00 +0530

    Seperating Abstract from contrete: moved all the possible commom content in super abstract class

--- Implementaion/Implementation_CH_4.java
+++ Implementaion/Implementation_CH_4.java
@@ -5,11 +5,19 @@ javac Implementation_CH_4.java -d ClassFiles
 java -cp ClassFiles OOD.Implementation_CH_4
 */
 abstract class Bicycle{
-	public String size;
-	public Bicycle(String size){
+	public String size,chain;
+	double tire_size;
+	public Bicycle(String size,String chain,double tire_size){
 		this.size=size;
+		this.tire_size=tire_size;
+		this.chain = chain;
 	}
-	public abstract String spares();
+	public String size(){
+		return size;
+	}
+	public String spares(){
+		return " chain : "+chain+" tire_size : "+tire_size;
+	}
 }
 class RoadBike extends Bicycle
 {
@@ -17,38 +25,29 @@ class RoadBike extends Bicycle
 	private String chain,tape_color;
     protected double tire_size;
 	public RoadBike(String size,String tape_color){
-		super(size);
+		
+		super(size,"10-speed",23);
 		this.tape_color = tape_color;
-		this.chain = "10-speed";
-		this.tire_size = 23;
-	}
-	public String size(){
-		return size;
 	}
 	public String tape_color(){
 		return tape_color;
 	}
 	public String spares(){
-		return " chain : "+chain+" tire_size : "+tire_size+" tape_color : "+ tape_color;
+		return super.spares()+" tape_color : "+ tape_color;
 	}
 	
 
 }
 class MountainBike extends Bicycle
 {
-	private String chain,front_shock,rear_shock;
+	private String front_shock,rear_shock;
 	double tire_size;
 	public MountainBike(String size,String front_shock,String rear_shock)
 	{
-		super(size);
+		super(size,"20-speed",2.1);
 		this.front_shock=front_shock;
 		this.rear_shock=rear_shock;
-		this.tire_size = 2.1;
-		this.chain="10-speed";
 		
-	}
-	public String size(){
-		return size;
 	}
 	public String front_shock(){
 		return front_shock;
@@ -58,7 +57,7 @@ class MountainBike extends Bicycle
 	}
 	public String spares(){
 		
-		return " chain : "+chain+" tire_size : "+tire_size+" rear_shock : "+rear_shock;
+		return super.spares()+" rear_shock : "+rear_shock;
 	}
 }
 public class Implementation_CH_4{
@@ -75,7 +74,7 @@ public class Implementation_CH_4{
 		// Output:
 		// MounttainBike size :S
 		// MountainBike spares :
-		//  chain : 10-speed tire_size : 2.1 rear_shock : Fox
+		//  chain : 20-speed tire_size : 2.1 rear_shock : Fox
 		// RoadBike size :M
 		// RoadBike spares :
 		//  chain : 10-speed tire_size : 23.0 tape_color : red
