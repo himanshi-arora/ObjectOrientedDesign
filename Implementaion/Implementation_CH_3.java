7445d63355806c8f3635df0edf6aecb56a269a69
Himi Arora <himanshi07a@gmail.com>
2020-07-13 02:03:12 +0530

    FINAL GEAR AND WHEEL CLASS: After removing dependencies and following single responsibilities

--- Implementaion/Implementation_CH_3.java
+++ Implementaion/Implementation_CH_3.java
@@ -25,6 +25,12 @@ class Gear{
 	private int not_chainring;  //number of teeth in chaining
 	private int not_cog;		//number of teeth in cog
 	private Wheel wheel;
+
+	public Gear(int not_chainring, int not_cog){
+		this.not_chainring=not_chainring;
+		this.not_cog=not_cog;
+		this.wheel=null;
+	}
 	public Gear(int not_chainring, int not_cog,Wheel wheel){
 		this.not_chainring=not_chainring;
 		this.not_cog=not_cog;
@@ -58,13 +64,25 @@ public class Implementation_CH_3{
 	public static void main(String[] args)
 	{
 		
-		Gear object1=new Gear(52,11,(new Wheel(26,1.5)));
+		Gear object1=new Gear(52,11,new Wheel(26,1.5));
 		System.out.println("Gear_Inches :"+ object1.gear_inches()); 
 
 		Gear object2=new Gear(52,11,26,1.25);
 		System.out.println("Gear_Inches :"+ object2.gear_inches()); 
+
+		Wheel wheel=new Wheel(26,1.5);
+		System.out.println("Wheel circumference :" + wheel.circumference());
+		Gear object3=new Gear(52,11,wheel);
+		System.out.println("Gear_Inches :"+ object3.gear_inches()); 
+
+		Gear object4=new Gear(52,11);
+		System.out.println("Ratio :"+ object4.ratio());
+		
 		// Output:
 		// Gear_Inches :137.0909090909091
 		// Gear_Inches :134.72727272727275
+		// Wheel circumference :91.106186954104
+		// Gear_Inches :137.0909090909091
+		// Ratio :4.7272727272727275
 	}
 }
