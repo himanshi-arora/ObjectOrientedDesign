bddffdbece46d92641105e76ecdae9ba9db0445f
Himi Arora <himanshi07a@gmail.com>
2020-07-13 01:19:34 +0530

    FINAL CLASS OF CHAPTER 2: Converting Wheel inner class to an independent Wheel class, now both class have a single reponsibility

--- Implementaion/Implementation_CH_2.java
+++ Implementaion/Implementation_CH_2.java
@@ -4,59 +4,70 @@ javac -version
 javac Implementation_CH_2.java -d ClassFiles
 java -cp ClassFiles OOD.Implementation_CH_2
 */
+class Wheel {
+
+	private double diameterOfRim;
+	private double diameterOfTire;
+
+	Wheel( double diameterOfRim, double diameterOfTire){
+		this.diameterOfRim = diameterOfRim;
+		this.diameterOfTire = diameterOfTire;
+	}
+
+	public double diameter(){
+		return (diameterOfRim + (diameterOfTire *2));
+	}	
+	public double circumference(){
+        return Math.PI * diameter();
+    }
+}
 class Gear{
-		private int not_chainring;  //number of teeth in chaining
-		private int not_cog;		//number of teeth in cog
-		private Wheel wheel;
+	private int not_chainring;  //number of teeth in chaining
+	private int not_cog;		//number of teeth in cog
+	private Wheel wheel;
 
-		class Wheel {
-
-			private float diameterOfRim;
-			private float diameterOfTire;
-
-			Wheel( float diameterOfRim, float diameterOfTire){
-				this.diameterOfRim = diameterOfRim;
-				this.diameterOfTire = diameterOfTire;
-			}
-
-			public float diameter(){
-				return (diameterOfRim + (diameterOfTire *2));
-			}	
-		}
-		
-		public Gear(int not_chainring, int not_cog,float diameterOfRim,float diameterOfTire){
-			this.not_chainring=not_chainring;
-			this.not_cog=not_cog;
-			this.wheel=new Wheel(diameterOfRim,diameterOfTire);
-		}
-		private int not_cog()
-		{
-			return not_cog;
-		}
-		private int not_chainring()
-		{
-			return not_chainring;
-		}
-		public float ratio(){
-			return (float)not_chainring()/(float) not_cog();
-		}
-		
-		public float gear_inches(){
-			return this.ratio() * wheel.diameter();
-		}
+	public Gear(int not_chainring, int not_cog){
+		this.not_chainring=not_chainring;
+		this.not_cog=not_cog;
+		this.wheel=null;
+	}
+	public Gear(int not_chainring, int not_cog, Wheel wheel){
+		this.not_chainring=not_chainring;
+		this.not_cog=not_cog;
+		this.wheel=wheel;
+	}
+	private int not_cog()
+	{
+		return not_cog;
+	}
+	private int not_chainring()
+	{
+		return not_chainring;
+	}
+	public double ratio(){
+		return (double)not_chainring()/(double) not_cog();
+	}
+	
+	public double gear_inches(){
+		return this.ratio() * wheel.diameter();
+	}
 }
 public class Implementation_CH_2{
 	public static void main(String[] args)
 	{
 		
-		Gear object1=new Gear(52,11,26,1.5f);
+		Wheel wheel=new Wheel(26,1.5);
+		System.out.println("Wheel circumference :" + wheel.circumference());
+
+		Gear object1=new Gear(52,11,wheel);
 		System.out.println("Gear_Inches :"+ object1.gear_inches()); 
-		Gear object2=new Gear(52,11,24,1.25f);
-		System.out.println("Gear_Inches :"+ object2.gear_inches());
+
+		Gear object2=new Gear(52,11);
+		System.out.println("Ratio :"+ object2.ratio());
 	
 		// Output:
-		// Gear_Inches: 137.0909
-		// Gear_Inches: 125.27272
-
+		// Wheel circumference :91.106186954104
+		// Gear_Inches :137.0909090909091
+		// Ratio :4.7272727272727275
 	}
 }
